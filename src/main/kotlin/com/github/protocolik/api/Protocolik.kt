ad112dad73be8c356192c3047fc106109c2428b1
XjCyan1de <xjcyan1de@yandex.ru>
2020-03-17 11:02:34 +0300

    Default Server Fix

--- src/main/kotlin/com/github/protocolik/api/Protocolik.kt
+++ src/main/kotlin/com/github/protocolik/api/Protocolik.kt
@@ -1,15 +1,16 @@
-@file:JvmName("Protocolik")
-
 package com.github.protocolik.api
 
 import com.github.protocolik.api.handler.PacketHandler
 import com.github.protocolik.api.handler.PacketHandlerManager
 import com.github.protocolik.api.protocol.Packet
+import java.io.File
 import java.util.logging.Logger
 
 object Protocolik : PacketHandlerManager {
     val log: Logger = Logger.getLogger("Protocolik")
+    var dataFolder: File = File("")
     override val handlers = HashMap<Class<out Packet>, MutableList<PacketHandler<out Packet>>>()
 }
 
 val log get() = Protocolik.log
+val dataFolder get() = Protocolik.dataFolder
